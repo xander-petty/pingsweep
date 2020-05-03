e6526e526c67136404f705fb9a111a6e71e43ba3
xander.petty <xander.petty@protonmail.com>
2020-05-03 07:23:03 -0500

    Rebuilt with packet crafting

--- pingsweep.py
+++ pingsweep.py
@@ -1,59 +1,72 @@
 """
-This is a scalable thread based ping tool 
-
-Required Modules:
-ipaddress
-pprint
-pythonping
+This is a test of building a multithreaded ping tool with
+manual packet crafting and thread daemons
 """
 __author__ = 'Xander Petty'
 __contact__ = 'Alexander.Petty@williams.com'
-__version__ = '0.0'
+__version__ = '1.0'
 
-from ipaddress import ip_network
-from pythonping import ping 
+from ipaddress import ip_network, ip_address
+from scapy.all import IP, ICMP, sr1
+from pprint import pprint 
 from threading import Thread, Lock 
 from queue import Queue 
-from pprint import pprint 
+import sys 
+import os 
+from time import sleep 
 
 thread_lock = Lock()
-q = Queue()
+receive_q = Queue()
+output = []
+all_threads = [] # For debugging purposes. Daemon's close with the process 
 
-def locked_ping(ip, count):
-    test = ping(ip, count=count)
+##########################
+# network = '192.168.0.0/26'
+# ping_count = 4
+# max_threads = 100
+network = input('Enter network with CIDR: ')
+ping_count = int(input('Enter the number of ping retries: '))
+max_threads = int(input('Enter the maximum number of threads: '))
+##########################
+
+def make_packet(ip):
+    packet = IP(dst=str(ip), ttl=20)/ICMP()
+    receive_q.put(packet)
+
+def locked_ping(packet):
+    print(str(f'Pinging: {packet.dst}'))
+    reply = sr1(packet, timeout=2, verbose=False, retry=ping_count)
     with thread_lock:
-        results = {
-            'IP': ip,
-            'Online': test.success()
-        }
-        pprint(results)
+        # print(str(f'Pinging: {packet.dst}'))
+        if reply != None:
+            output.append(reply)
 
-def variable_map(ip_list, ping_count):
-    mapped = []
-    for ip in ip_list:
-        mapped.append([str(ip), ping_count])
-    return mapped
+def receive_daemon():
+    while True:
+        packet = receive_q.get()
+        locked_ping(packet)
+        receive_q.task_done()
 
-def thread_task():
-    while True: # Used as daemon 
-        ip, count = q.get()
-        locked_ping(ip, count)
-        q.task_done() 
-
-def main(network, ping_count, max_threads):
-    ip_list = list(ip_network(network))
-    mapped = variable_map(ip_list, ping_count)
-    for t in range(max_threads):
-        thread = Thread(target=thread_task)
-        thread.daemon = True 
-        thread.start() 
-    for ping_set in mapped:
-        q.put(ping_set)
-    q.join() 
+def main(network, max_threads):
+    all_hosts = list(ip_network(network))
+    for num in range(max_threads):
+        t = Thread(target=receive_daemon)
+        t.daemon = True 
+        t.start()
+    for ip in all_hosts:
+        make_packet(ip)
+    receive_q.join()
 
 
-network = input('Please enter a network and cidr pair: ')
-count = int(input('How many pings each?: '))
-max_threads = int(input('What is the maximum thread count?: '))
+main(network, max_threads)
 
-main(network, count, max_threads)
+online = []
+for ip in output:
+    online.append(ip_address(ip.src))
+online = sorted(online)
+wait = ((len(list(ip_network(network))) * 2) / max_threads) * ping_count
+# sleep(wait)
+print('#############################################')
+print('###                 ONLINE                ###')
+print('#############################################')
+pprint(online)
